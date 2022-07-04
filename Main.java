35f9e9fb35bd845be69e8ad6196c3539297b4f17
Kravchenko Alexey <108604157+A-Bell-95@users.noreply.github.com>
2022-07-04 12:30:15 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,72 +3,77 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Main extends JPanel {
 
     final int BF_WIDTH = 576;
     final int BF_HEIGHT = 576;
-    // 1 - Up, 2 - Down, 3 - Left, 4 - right
-    int direction = 1;
+    final int OBJECT_SIZE = 64;
+
+    final int UP = 1;
+    final int DOWN = 2;
+    final int LEFT = 3;
+    final int RIGHT = 4;
+
+    String[][] Objects ={
+            {"W","W","S","G","G","R","R","R","B"},
+            {"W","W","S","B","B","R","R","B","B"},
+            {"S","S","S","B","B","R","R","R","B"},
+            {"R","B","R","R","B","B","B","R","R"},
+            {"R","B","R","R","R","R","R","R","B"},
+            {"R","B","R","R","B","R","R","R","R"},
+            {"R","R","R","R","R","R","R","G","G"},
+            {"W","S","R","B","B","R","R","S","W"},
+            {"W","W","R","R","R","R","R","S","W"}
+        };
+    int direction = UP;
 
     int bulletX = 320;
     int bulletY = 320;
 
-    int x = 256;
-    int y = 256;
+    int tankX = 256;
+    int tankY = 256;
 
     void move(int direction) throws Exception{
         this.direction = direction;
 
-        if (direction == 1){
-            y--;
-        } else if (direction == 2) {
-            y++;
-        } else if (direction == 3) {
-            x--;
-        } else if (direction == 4) {
-            x++;
+        if (direction == UP){
+            tankY--;
+        } else if (direction == DOWN) {
+            tankY++;
+        } else if (direction == LEFT) {
+            tankX--;
+        } else if (direction == RIGHT) {
+            tankX++;
         }
         Thread.sleep(33);
         repaint();
     }
 
+    void fire() throws Exception{
+        bulletX = tankX + 25;
+        bulletY = tankY + 25;
+
+        while (bulletX > 0 && bulletX < BF_WIDTH && bulletY > 0 && bulletY < BF_HEIGHT){
+            switch (direction) {
+                case UP -> bulletY--;
+                case DOWN -> bulletY++;
+                case LEFT -> bulletX--;
+                case RIGHT -> bulletX++;
+            }
+            Thread.sleep(10);
+             repaint();
+        }
+        bulletX = -100;
+        bulletY = -100;
+        repaint();
+    }
+
     void runTheGame() throws Exception{
-        while (y != 0) {
-            move(1);
-        }
+        fire();
+
     }
-    void moveUp() throws Exception{
-        direction = 1;
-        while (y != 0) {
-            y--;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
-    void moveDown() throws Exception{
-        direction = 2;
-        while (y != BF_HEIGHT - 64) {
-            y++;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
-    void moveLeft() throws Exception{
-        direction = 3;
-        while (x != 0) {
-            x--;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
-    void moveRight() throws Exception{
-        direction = 4;
-        while (x != BF_WIDTH - 64) {
-            x++;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
+
 
     public static void main(String[] args) throws Exception {
         Main main = new Main();
@@ -86,25 +91,45 @@ public class Main extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.RED);
-        g.fillRect(x,y,64,64);
-        g.setColor(Color.WHITE);
-        g.drawLine(x + 37,y + 50,x + 42,y + 60);
-        g.drawLine(x + 37,y + 50,x + 32,y + 60);
-        g.drawLine(x + 32,y + 60,x + 27,y + 50);
-        g.drawLine(x + 27,y + 50,x + 22,y + 60);
-        g.setColor(Color.ORANGE);
-        if (direction == 1){
-            g.fillRect(x + 20,y,24,34);
-        } else if (direction == 2) {
-            g.fillRect(x + 20,y + 30,24,34);
-        } else if (direction == 3) {
-            g.fillRect(x,y + 20,34,24);
-        } else if (direction == 4) {
-            g.fillRect(x + 30 ,y + 20,34,24);
+        Color cc = new Color(146, 37, 37);
+        Color cf = new Color(190, 197, 197);
+        Color cg = new Color(10,140,15);
+        //Draw objects
+        for (int i = 0; i < Objects.length; i++) {
+            for (int j = 0; j < Objects.length; j++) {
+                switch (Objects[i][j]) {
+                    case "W" -> g.setColor(Color.CYAN);
+                    case "B" -> g.setColor(cc);
+                    case "R" -> g.setColor(cf);
+                    case "G" -> g.setColor(cg);
+                    case "S" -> g.setColor(Color.ORANGE);
+                }
+                g.fillRect(OBJECT_SIZE * i, OBJECT_SIZE * j, OBJECT_SIZE, OBJECT_SIZE);
+            }
         }
+        //Draw tank
+        g.setColor(Color.RED);
+        g.fillRect(tankX, tankY, OBJECT_SIZE, OBJECT_SIZE);
+        g.setColor(Color.WHITE);
+        g.drawLine(tankX + 37, tankY + 50, tankX + 42, tankY + 60);
+        g.drawLine(tankX + 37, tankY + 50, tankX + 32, tankY + 60);
+        g.drawLine(tankX + 32, tankY + 60, tankX + 27, tankY + 50);
+        g.drawLine(tankX + 27, tankY + 50, tankX + 22, tankY + 60);
+        //Draw Gun
+        g.setColor(Color.YELLOW);
+        if (direction == UP) {
+            g.fillRect(tankX + 20, tankY, 24, 34);
+        } else if (direction == DOWN) {
+            g.fillRect(tankX + 20, tankY + 30, 24, 34);
+        } else if (direction == LEFT) {
+            g.fillRect(tankX, tankY + 20, 34, 24);
+        } else if (direction == RIGHT) {
+            g.fillRect(tankX + 30, tankY + 20, 34, 24);
+        }
+        //Draw bullet
         g.setColor(Color.BLACK);
-        g.fillRect(bulletX, bulletY,14,14);
+        g.fillRect(bulletX, bulletY, 14, 14);
 
     }
 }
+
